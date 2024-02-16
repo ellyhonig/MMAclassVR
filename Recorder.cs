2b40a2ec4308c43769242ea55de7612d70ebc620
ellyhonig <39608375+ellyhonig@users.noreply.github.com>
2024-02-16 13:26:48 -0500

    added saving to JSON file

--- Recorder.cs
+++ Recorder.cs
@@ -1,19 +1,18 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Recorder : MonoBehaviour
 {
     private player PlayerToRecord;
     private MirroredPlayer mirroredPlayer;
-
     private Record currentRecord;
     private delegate void UpdateDelegate();
     private UpdateDelegate currentUpdate;
+    private string saveFilePath;
 
-    void Start()
+    void Awake()
     {
-        // Fetch the player object from the attached person class
         var personComponent = GetComponent<person>();
         if (personComponent != null)
         {
@@ -23,6 +22,9 @@ public class Recorder : MonoBehaviour
 
         currentRecord = new Record();
         currentUpdate = null;
+
+        // Initialize save file path
+        saveFilePath = Path.Combine(Application.persistentDataPath, "recording.json");
     }
 
     void Update()
@@ -32,15 +34,12 @@ public class Recorder : MonoBehaviour
 
     public void StartRecording()
     {
-        mirroredPlayer.SetUpdateTransformsMethod(mirroredPlayer.UpdateDummyTransforms);
         currentRecord = new Record();
         currentUpdate = RecordFrame;
     }
 
     public void PauseRecording()
     {
-        mirroredPlayer.SetUpdateTransformsMethod(mirroredPlayer.UpdateDummyTransforms);
-        Debug.Log("paused");
         currentUpdate = null;
     }
 
@@ -48,7 +47,6 @@ public class Recorder : MonoBehaviour
     {
         currentRecord.currentFrame = 0;
         currentUpdate = PlayFrame;
-        mirroredPlayer.SetUpdateTransformsMethod(null);
     }
 
     private void RecordFrame()
@@ -56,7 +54,6 @@ public class Recorder : MonoBehaviour
         var frame = new Frame();
         frame.CapturePlayerState(PlayerToRecord);
         currentRecord.frames.Add(frame);
-        Debug.Log("recording");
     }
 
     private void PlayFrame()
@@ -69,61 +66,81 @@ public class Recorder : MonoBehaviour
 
         var frame = currentRecord.frames[currentRecord.currentFrame];
         frame.ApplyToPlayer(mirroredPlayer.mirroredPlayer);
-        Debug.Log("applying");
         currentRecord.currentFrame++;
     }
-}
 
-public class Record
-{
-    public List<Frame> frames;
-    public int currentFrame;
-
-    public Record()
+    public void SaveRecording()
     {
-        frames = new List<Frame>();
-        currentFrame = 0;
+        string json = JsonUtility.ToJson(currentRecord, true);
+        File.WriteAllText(saveFilePath, json);
+    }
+
+    public void LoadRecording()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+            currentRecord = JsonUtility.FromJson<Record>(json);
+        }
     }
 }
-
-
+[System.Serializable]
 public class Frame
 {
-    private Dictionary<string, (Vector3 position, Quaternion rotation)> data;
+    public List<FrameData> data = new List<FrameData>();
 
-    public Frame()
-    {
-        data = new Dictionary<string, (Vector3, Quaternion)>();
-    }
+    public Frame() { }
 
     public void CapturePlayerState(player playerToCapture)
     {
-        data["hmd"] = (playerToCapture.hmd.localPosition, playerToCapture.hmd.localRotation);
-        data["conR"] = (playerToCapture.conR.localPosition, playerToCapture.conR.localRotation);
-        data["conL"] = (playerToCapture.conL.localPosition, playerToCapture.conL.localRotation);
-        data["kneeConR"] = (playerToCapture.kneeConR.localPosition, playerToCapture.kneeConR.localRotation);
-        data["kneeConL"] = (playerToCapture.kneeConL.localPosition, playerToCapture.kneeConL.localRotation);
+        // Clear existing data
+        data.Clear();
+
+        // Populate the list with frame data
+        data.Add(new FrameData("hmd", playerToCapture.hmd.localPosition, playerToCapture.hmd.localRotation));
+        data.Add(new FrameData("conR", playerToCapture.conR.localPosition, playerToCapture.conR.localRotation));
+        data.Add(new FrameData("conL", playerToCapture.conL.localPosition, playerToCapture.conL.localRotation));
+        data.Add(new FrameData("kneeConR", playerToCapture.kneeConR.localPosition, playerToCapture.kneeConR.localRotation));
+        data.Add(new FrameData("kneeConL", playerToCapture.kneeConL.localPosition, playerToCapture.kneeConL.localRotation));
     }
 
     public void ApplyToPlayer(player playerToApply)
     {
-        // Mapping from part names to their respective Transform references
-        var bodyParts = new Dictionary<string, Transform>
+        foreach (FrameData frameData in data)
         {
-            ["hmd"] = playerToApply.hmd,
-            ["conR"] = playerToApply.conR,
-            ["conL"] = playerToApply.conL,
-            ["kneeConR"] = playerToApply.kneeConR,
-            ["kneeConL"] = playerToApply.kneeConL
-        };
-
-        foreach (var part in data)
-        {
-            if (bodyParts.TryGetValue(part.Key, out Transform bodyPartTransform))
+            // You'll need to implement this part based on how you can map `frameData.partName` to your player's transforms
+            // For example:
+            Transform targetTransform = playerToApply.GetTransformByName(frameData.partName);
+            if (targetTransform != null)
             {
-                bodyPartTransform.localPosition = part.Value.position;
-                bodyPartTransform.localRotation = part.Value.rotation;
+                targetTransform.localPosition = frameData.position;
+                targetTransform.localRotation = frameData.rotation;
             }
         }
+    }
+}
+[System.Serializable]
+public class FrameData
+{
+    public string partName;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public FrameData(string name, Vector3 pos, Quaternion rot)
+    {
+        partName = name;
+        position = pos;
+        rotation = rot;
+    }
+}
+[System.Serializable]
+public class Record
+{
+    public List<Frame> frames = new List<Frame>();
+    public int currentFrame;
+
+    public Record()
+    {
+        currentFrame = 0;
     }
 }
